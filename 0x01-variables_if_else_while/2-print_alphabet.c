a39f4ff76e4d5c6f8e039f994ccfef95599a091c
Diego Monfort <diegomonfort12346@gmail.com>
2021-10-01 10:29:50 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+*main -Alfhabet print
+*return: 0
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar (ch);
+	putchar ('\n');
+	return (0);
+
+
+}
