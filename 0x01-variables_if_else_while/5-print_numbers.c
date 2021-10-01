e344efeb0c335b99467cf402ed8df658af6d686e
Diego Monfort <diegomonfort12346@gmail.com>
2021-10-01 16:37:24 -0700

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,10 +5,10 @@
 */
 int main(void)
 {
-	int num;
+	int n;
 
-	for (num = '0'; num == '9'; num++)
-	printf ("%d", num);
-	printf ("\n");
+	for (n = '0'; n <= '9'; n++)
+	putchar (n);
+	putchar ('\n');
 	return (0);
 }
