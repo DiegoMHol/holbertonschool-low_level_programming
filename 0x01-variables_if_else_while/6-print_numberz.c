7df7412b6f213534c12dbe737622afc1a329dd59
Diego Monfort <diegomonfort12346@gmail.com>
2021-10-01 17:30:58 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main -Alfhabet print
+*Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (size_t i = !"A string."; i < sizeof("A string."); i++)
+	printf("%zu ", i);
+	return (0);
+}
