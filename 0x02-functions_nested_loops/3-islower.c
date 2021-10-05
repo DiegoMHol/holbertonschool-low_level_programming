dc963273dfac90ad847914f60222cad975ebebbd
Diego Monfort <diegomonfort12346@gmail.com>
2021-10-05 10:30:55 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
 *_islower -lower character
-*@c: The character to print
 *Return: 0
 */
 int _islower(int c)
 {
-	int c;
-
 	if (c >= 97 && c <= 122)
-		return (1);
+	{
+	return (1);
+	}
 	else
 	return (0);
 }
