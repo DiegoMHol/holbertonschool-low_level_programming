041ade357535fb495647c38c4bdaec00750749c0
Diego Monfort <diegomonfort12346@gmail.com>
2021-10-05 14:26:12 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*print_last_digit - las digit print
+*@d: a
+*Return: 0
+*/
+int print_last_digit(int d)
+{
+	int lastDigit = d % 10;
+
+	_putchar(lastDigit);
+	return (0);
+}
