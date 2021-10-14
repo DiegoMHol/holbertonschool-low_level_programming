95053bd5548e0dac93d273de151f631cebbc31a5
Diego Monfort <diegomonfort12346@gmail.com>
2021-10-14 10:48:24 -0700

    two strings concatened

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,11 +13,11 @@ char *_strcat(char *dest, char *src)
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	for (j = 0; src[j] != '\0'; j++, i++)
+	for (j = 0; src[j] != '\0'; j++)
 	{
-		dest[i] = src[j];
+		dest[i + j] = src[j];
 	}
-	dest[i] = '\0';
+	dest[i + j] = '\0';
 
-	return (0);
+	return (dest);
 }
