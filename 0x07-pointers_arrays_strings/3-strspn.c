f0702112c6b41488bf36dd1a4244c3b2aee483c8
Diego Monfort <diegomonfort12346@gmail.com>
2021-10-19 17:26:41 -0700

    strspen

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,5 +7,19 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-	
+	unsigned int i = 0;
+	unsigned int j;
+	unsigned int k = 0;
+
+	for (; s[i] && s[i] != ' '; i++)
+	{
+		j = 0;
+
+		for (; accept[j]; j++)
+		{
+			if (accept[j] == s[i])
+				k++;
+		}
+	}
+	return (k);
 }
