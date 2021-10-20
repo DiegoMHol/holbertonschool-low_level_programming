7d2eb7c0cbf3e04a6c3e72a8f5d4f4e07754658c
Diego Monfort <diegomonfort12346@gmail.com>
2021-10-20 09:31:36 -0700

    length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,15 +6,15 @@
 */
 int _strlen_recursion(char *s)
 {
-	int i;
+	int i = 0;
 
 	if (*s == 0)
 	{
-		return (0);
+		return ('\0');
 	}
 	else
 	{
-		i = 0;
+		i++;
 		i += _strlen_recursion(s + 1);
 	}
 	return (i);
