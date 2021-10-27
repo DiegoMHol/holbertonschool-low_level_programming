0cb1b59e6199c2f4d6b2f646533e10b1cdaba8d7
Diego Monfort <diegomonfort12346@gmail.com>
2021-10-27 14:44:18 -0700

    advanced

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/*
+*argstostr -concatenates arguments
+*@ac: int
+*@av: char
+*Return: 0
+*/
+char *argstostr(int ac, char **av)
+{
+	char *p;
+
+	return (p);
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*strtow -splits two sring inot words
+*@str: char
+*Return: 0
+*/
+char **strtow(char *str)
+{
+	return (0);
+}
