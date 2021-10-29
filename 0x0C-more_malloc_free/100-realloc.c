c8fdc611ceb88af621db2285cefd991ed1fc7180
Diego Monfort <diegomonfort12346@gmail.com>
2021-10-28 18:13:51 -0700

    realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*_realloc - realoc memory
+*@ptr: void
+*@old_size: unsigned int
+*@new_size: unsigned int
+*Return: 0
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+}
