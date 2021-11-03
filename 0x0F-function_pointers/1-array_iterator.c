a855fc5bc3f3d334a0a635bb43f6ac61bf696675
Diego Monfort <diegomonfort12346@gmail.com>
2021-11-03 13:00:36 -0700

    Iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+*array_interator -entry
+*@array: int
+*@size: size_t
+*@action: action
+*Return: 0
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		action(*(array + i));
+	}
+}
