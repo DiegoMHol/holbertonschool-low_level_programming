039f92fcbdd45353ae8bf7a73df2263916204d4d
Diego Monfort <diegomonfort12346@gmail.com>
2021-11-03 19:45:44 -0700

    search for int

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,16 +11,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL || size <= 0)
+	if (array != NULL && cmp != NULL && size > 0)
 	{
-		return (-1);
-	}
-
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(*(array + 1)))
+		i = 0;
+		while (i < size)
 		{
-			return (i);
+			if (cmp(array[i]) == 1)
+			{
+				return (i);
+			}
+			i++;
 		}
 	}
 	return (-1);
