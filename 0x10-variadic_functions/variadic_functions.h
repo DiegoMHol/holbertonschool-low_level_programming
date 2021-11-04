9102c9ee394c663200a6fc132e15ff267b7fb328
Diego Monfort <diegomonfort12346@gmail.com>
2021-11-04 13:24:18 -0700

    H

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef VARIDIC_FUNCTIONS
 #define VARIDIC_FUNCTIONS
 #include <stdarg.h>
+void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 #endif
