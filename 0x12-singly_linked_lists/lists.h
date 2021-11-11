467134f685176770f725c0c6501503693141e9e2
Diego Monfort <diegomonfort12346@gmail.com>
2021-11-11 12:17:57 -0800

    added prototype task 2

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LIST_H
 #define LIST_H
+#include <string.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,6 +17,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
 #endif
