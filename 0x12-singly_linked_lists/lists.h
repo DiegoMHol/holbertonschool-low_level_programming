ed38d113a7e990247a5f29e175775c759638b793
Diego Monfort <diegomonfort12346@gmail.com>
2021-11-11 11:55:09 -0800

    List.h

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#ifndef LIST_H
+#define LIST_H
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*struct list_s -sinlge linked list
+*@str: string
+*@len: length of string
+*@next: next node point
+*/
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+#endif
