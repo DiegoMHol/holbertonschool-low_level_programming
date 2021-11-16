b3d00b419a3a5ef025888ed93bf23146bbc843fd
Diego Monfort <diegomonfort12346@gmail.com>
2021-11-16 11:25:55 -0800

    delate head

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,16 +9,15 @@ int pop_listint(listint_t **head)
 	listint_t *tmp;
 	int n2;
 
+	if (head != NULL)
+	{
 	tmp = (*head)->next;
 	n2 = (*head)->n;
-
-	if (*head == NULL)
-	{
-		return (0);
-	}
-	else
 	free(*head);
 	*head = tmp;
 
 	return (n2);
+	}
+
+	return (0);
 }
