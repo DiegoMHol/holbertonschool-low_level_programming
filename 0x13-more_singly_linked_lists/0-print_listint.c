227aceca4b671a5335ece1710618b3e42acfdcde
Diego Monfort <diegomonfort12346@gmail.com>
2021-11-16 09:42:05 -0800

    Return all elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,18 +8,15 @@ size_t print_listint(const listint_t *h)
 {
 	size_t len = 0;
 
-	for (len = 0; 1 == 1; len++)
+	if (h == NULL)
 	{
-		if (h == NULL)
-		{
-			break;
-		}
-		while (h)
-		{
-			printf("[%d]\n", h->n);
-			h = h->next;
-			len++;
-		}
+		return (0);
+	}
+	while (h)
+	{
+		printf("[%d]\n", h->n);
+		h = h->next;
+		len++;
 	}
 	return (len);
 }
