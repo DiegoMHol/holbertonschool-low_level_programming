41855f7c1cf0851a2538005f045cbecf735c9e1a
Diego Monfort <diegomonfort12346@gmail.com>
2021-11-16 11:09:03 -0800

    delate head

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+*pop_listint -delate head node
+*@head: head
+*Return: 0
+*/
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int n2;
+
+	tmp = (*head)->next;
+	n2 = (*head)->n;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	free(*head);
+	*head = tmp;
+
+	return (n2);
+}
