464a7344c1981730c96ad9d4490db085a4ab4531
Diego Monfort <diegomonfort12346@gmail.com>
2021-11-18 16:13:40 -0800

    Binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,21 +6,21 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i;
-	int j;
+	int i;
+	unsigned int j = 0;
 
 	if (!b)
 	{
 		return (0);
 	}
 
-	for (j = 0; b[j]; j++)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[j] < '0' || b[j] > '1')
+		if (b[i] < '0' || b[i] > '1')
 		{
 			return (0);
 		}
-		i = 2 * i + (b[j] - '0');
+		j = 2 * j + (b[i] - '0');
 	}
-	return (i);
+	return (j);
 }
