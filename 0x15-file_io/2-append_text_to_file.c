eb829851202863b03aeca96971f7c5a0641356b9
Diego Monfort <diegomonfort12346@gmail.com>
2021-11-22 14:29:56 -0800

    Append

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+*append_text_to_file -appends text at the end of file
+*@filename: name of file
+*@text_content: string to add
+*Return: 1
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int append, l, sts;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	append = open(filename, O_WRONLY | O_APPEND);
+
+	if (append == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+
+	for (l = 0; text_content[l]; l++)
+	{
+		;
+	}
+
+	sts = write(append, text_content, l);
+	close(append);
+	return (sts == -1 ? -1 : 1);
+}
