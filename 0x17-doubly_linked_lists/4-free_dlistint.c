b51108855eaa81e6310b9c81cd05c54d1f0dae8a
Diego Monfort <diegomonfort12346@gmail.com>
2022-01-09 15:01:26 -0800

    free

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *free_dlistint
+ *@head: point
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp = head;
+
+	while (head != NULL)
+	{
+		head = head->next;
+		free(tmp);
+		tmp = head;
+	}
+	free(head);
+}
