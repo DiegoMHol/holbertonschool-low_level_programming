4fae399c4f8c16bc49083af481a0942b153154de
DiegoMHol <diegomonfort1236@gmail.com>
2022-05-04 18:33:00 -0700

    [FIX OUTPUT ERROR]

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,24 +1,5 @@
 #include "search_algos.h"
 /**
- * printFunc - Print array
- * @array: pointer to 1st elem
- * @size: number of elem
- */
-void printFunc(int *array, size_t size)
-{
-	size_t i;
-
-	printf("Searching in array: ");
-	for (i = 0; i < size; i++)
-	{
-		if (i == size - 1)
-			printf("%d\n", array[i]);
-		else
-			printf("%d, ", array[i]);
-	}
-}
-
-/**
  * binary_search - Binary search algo
  * @array: pointer to 1st elem
  * @size: number of elem
@@ -27,6 +8,7 @@ void printFunc(int *array, size_t size)
  */
 int binary_search(int *array, size_t size, int value)
 {
+	size_t i = 0;
 	size_t left = 0;
 	size_t right = size - 1;
 	size_t med;
@@ -34,22 +16,25 @@ int binary_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	printFunc(array, size);
 	while (left <= right)
 	{
 		med = (left + right) / 2;
-		if (value > array[med])
+		i = left;
+		printf("Searching in array: ");
+
+		for (; i <= right; i++)
 		{
+			if (i != left)
+				printf(", %d", array[i]);
+			else
+				printf("%d", array[i]);
+		}
+		printf("\n");
+
+		if (array[med] < value)
 			left = med + 1;
-			if (right >= 1)
-				printFunc(array + left, right + 1 - left);
-		}
-		else if (value < array[med])
-		{
+		else if (array[med] > value)
 			right = med - 1;
-			if (right >= 1)
-				printFunc(array + left, right + 1 - left);
-		}
 		else
 			return (med);
 	}
