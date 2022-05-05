1e48310e545672e7ea15a77c8bef170d20da9c64
DiegoMHol <diegomonfort1236@gmail.com>
2022-05-04 18:33:35 -0700

    [CHANGE]

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,11 +2,9 @@
 #define SEARCH_ALGOS_H
 
 #include <stdio.h>
-#include <math.h>
 
 /* Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-void printFunc(int *array, size_t size);
 
 #endif
